c91c027c2ba800cc208288e51501f110427d1fbe
rayleighRe1 <69469718+GiulioTriggiani@users.noreply.github.com>
2023-01-16 14:40:25 +0100

    SearchView updates

--- smart_assistant/lib/features/search/views/Search.dart
+++ smart_assistant/lib/features/search/views/Search.dart
@@ -5,9 +5,17 @@ import 'dart:core';
 import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
 import 'package:smart_assistant/features/chat_bot/models/responseParser.dart';
 import 'package:smart_assistant/features/dashboard/classes/Response.dart';
+
+import '../../../shared/res/colors.dart';
+import '../../../shared/res/typography.dart';
+import '../../dashboard/widgets/document.dart';
+import '../../dashboard/widgets/documentsList.dart';
+import '../../dashboard/widgets/machine_info.dart';
+import '../../dashboard/widgets/machine_stats.dart';
 
 class SearchView extends StatefulWidget {
   static List<String> docList = [];
@@ -46,7 +54,7 @@ class DataFromResponse {
 }
 
 class _SearchViewState extends State<SearchView> {
-  String searchValue = '';
+  String searchValue = MySearchDelegate().query;
   OggettoOggetto? oggetto;
 
   late Attivita attivita;
@@ -70,6 +78,7 @@ class _SearchViewState extends State<SearchView> {
     }
   }
 
+/*
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -95,13 +104,108 @@ class _SearchViewState extends State<SearchView> {
                 ),
                 child: Text('Drawer Header'),
               ),
-              ListTile(
-                  title: const Text('Item 1'),
-                  onTap: () => Navigator.pop(context)),
-              ListTile(
-                  title: const Text('Item 2'),
-                  onTap: () => Navigator.pop(context))
+
+              DocumentsList(
+                oggetto: oggetto!,
+                document: List.generate(
+                  SearchView.docList.length,
+                  (i) => Document(
+                    'Documento ' + SearchView.docList[i],
+                    SearchView.urlList[i],
+                  ),
+                ),
+              ),
+
             ])),
             body: Center(child: Text('Value: $searchValue'))));
+  } // Build
+*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: MySearchDelegate());
+              getAttivitas().whenComplete(() => fillDocList());
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(height: 32.h),
+            //Titolo al centro
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Documents',
+                  style: TextStyles.body.copyWith(
+                    fontSize: TextStyles.headline2.fontSize,
+                    color: SmartAssistantColors.primary,
+                  ),
+                )
+              ],
+            ),
+/*   Questo è commentato perchè manca la lista di documenti e da errore
+            DocumentsList(
+              oggetto: oggetto!,
+              document: List.generate(
+                SearchView.docList.length,
+                (i) => Document(
+                  'Documento ' + SearchView.docList[i],
+                  SearchView.urlList[i],
+                ),
+              ),
+            ),*/
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          if (query.isEmpty) {
+            close(context, null);
+          } else {
+            query = '';
+          }
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
   }
 }
