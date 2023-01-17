02fb67e9b833bb4de86768080994e1f6f03bdced
rayleighRe1 <69469718+GiulioTriggiani@users.noreply.github.com>
2023-01-17 17:31:27 +0100

    SearchView fix and update
    Co-Authored-By: Alessio Casolaro <65906352+AlessioCasolaro@users.noreply.github.com>
    Co-Authored-By: domenico d'alessandro <74026981+domenicodale@users.noreply.github.com>
    Co-Authored-By: Andrea Di Pierno <48768642+adipiz99@users.noreply.github.com>

--- smart_assistant/lib/features/search/views/Search.dart
+++ smart_assistant/lib/features/search/views/Search.dart
@@ -2,26 +2,29 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:core';
 
-import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
-import 'package:smart_assistant/features/chat_bot/models/responseParser.dart';
+import 'package:smart_assistant/features/search/classes/ResponseSearch.dart';
 import 'package:smart_assistant/features/dashboard/classes/Response.dart';
 
 import '../../../shared/res/colors.dart';
 import '../../../shared/res/typography.dart';
 import '../../dashboard/widgets/document.dart';
 import '../../dashboard/widgets/documentsList.dart';
-import '../../dashboard/widgets/machine_info.dart';
 import '../../dashboard/widgets/machine_stats.dart';
+
+OggettoOggetto? ogg;
+String searchValueRecived = "";
 
 class SearchView extends StatefulWidget {
   static List<String> docList = [];
   static List<String> urlList = [];
 
-  SearchView({Key? key, required OggettoOggetto oggetto}) : super(key: key);
+  SearchView({Key? key, required OggettoOggetto oggetto}) : super(key: key) {
+    ogg = oggetto;
+  }
   @override
   State<SearchView> createState() => _SearchViewState();
 }
@@ -43,169 +46,129 @@ Future<http.Response> getData(String titolo) {
 }
 
 class DataFromResponse {
-  static Future<ResponseDashboard> getDataLocally(
+  static Future<ResponseSearch> getDataLocally(
       BuildContext context, String titolo) async {
+    ResponseSearch responseSearch = ResponseSearch();
     final data = await getData(titolo);
-    final reportData = responseFromJsonDashboard(data.body);
+    final reportData = responseSearchFromJson(data.body);
 
-    log("Log Search" + data.body.toString());
-    return reportData;
+    return reportData!;
   }
 }
 
 class _SearchViewState extends State<SearchView> {
-  String searchValue = MySearchDelegate().query;
-  OggettoOggetto? oggetto;
+  //String searchValue = MySearchDelegate().query;
 
-  late Attivita attivita;
-  List<ContenutosOggetto> contenutoOggetto = [];
+  List<RicercaDocumentiTitolo> listaDocumenti = [];
   Future<void> getAttivitas() async {
-    await DataFromResponse.getDataLocally(context, searchValue).then((value) {
+    await DataFromResponse.getDataLocally(context, searchValueRecived)
+        .then((value) {
       setState(() {
-        attivita = value.data.attivitas[0];
-        oggetto = attivita.oggettoOggettos[0];
-        for (int i = 0; i < oggetto!.contenutosOggetto.length; i++) {
-          contenutoOggetto.add(oggetto!.contenutosOggetto[i]);
-        }
+        SearchView.docList = [];
+        SearchView.urlList = [];
       });
+      for (int i = 0; i < value.ricercaDocumentiTitolo!.length; i++) {
+        setState(() {
+          SearchView.docList
+              .add(value.ricercaDocumentiTitolo![i]!.titolo.toString());
+          SearchView.urlList.add(value
+              .ricercaDocumentiTitolo![i]!.riferimentoDocumentale
+              .toString());
+        });
+      }
     });
   }
 
   void fillDocList() {
-    for (int i = 0; i < contenutoOggetto.length; i++) {
-      SearchView.docList.add(contenutoOggetto[i].titolo);
-      SearchView.urlList.add(contenutoOggetto[i].riferimentoDocumentale);
+    for (int i = 0; i < listaDocumenti.length; i++) {
+      SearchView.docList.add(listaDocumenti[i].titolo!);
+      SearchView.urlList.add(listaDocumenti[i].riferimentoDocumentale!);
     }
   }
 
-/*
+  late TextEditingController _controller;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Example',
-        theme: ThemeData(primarySwatch: Colors.orange),
-        home: Scaffold(
-            appBar: EasySearchBar(
-              title: const Text('Example'),
-              onSearch: (value) => setState(() => searchValue = value),
-              actions: [
-                IconButton(
-                    icon: const Icon(Icons.person),
-                    onPressed: () {
-                      getAttivitas().whenComplete(() => fillDocList());
-                    })
-              ],
-            ),
-            drawer: Drawer(
-                child: ListView(padding: EdgeInsets.zero, children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text('Drawer Header'),
-              ),
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
 
-              DocumentsList(
-                oggetto: oggetto!,
-                document: List.generate(
-                  SearchView.docList.length,
-                  (i) => Document(
-                    'Documento ' + SearchView.docList[i],
-                    SearchView.urlList[i],
-                  ),
-                ),
-              ),
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
-            ])),
-            body: Center(child: Text('Value: $searchValue'))));
-  } // Build
-*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Search'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              showSearch(context: context, delegate: MySearchDelegate());
-              getAttivitas().whenComplete(() => fillDocList());
-            },
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.w),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SizedBox(height: 32.h),
-            //Titolo al centro
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Documents',
-                  style: TextStyles.body.copyWith(
-                    fontSize: TextStyles.headline2.fontSize,
-                    color: SmartAssistantColors.primary,
-                  ),
-                )
-              ],
-            ),
-/*   Questo è commentato perchè manca la lista di documenti e da errore
-            DocumentsList(
-              oggetto: oggetto!,
-              document: List.generate(
-                SearchView.docList.length,
-                (i) => Document(
-                  'Documento ' + SearchView.docList[i],
-                  SearchView.urlList[i],
-                ),
-              ),
-            ),*/
-          ]),
+        appBar: AppBar(
+          title: Text("Search Documents"),
+          backgroundColor: SmartAssistantColors.primary,
+          elevation: 0,
         ),
-      ),
-    );
-  }
-}
+        body: Column(
+          children: [
+            MachineStats(
+                macchina: ogg!,
+                box: const BoxDecoration(color: Color(0xFF1F75FE))),
+            const SizedBox(height: 10),
+            Container(
+              width: 600.h,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search your document',
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                      borderSide: BorderSide(color: Colors.blue)),
+                ),
+                controller: _controller,
+                onSubmitted: (searchValue) async {
+                  {
+                    setState(() {
+                      searchValueRecived = _controller.text;
+                    });
+                  }
+                  await getAttivitas();
+                },
+              ),
+            ),
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.w),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 32.h),
+                      //Titolo al centro
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Documents list',
+                            style: TextStyles.body.copyWith(
+                              fontSize: TextStyles.headline2.fontSize,
+                              color: SmartAssistantColors.primary,
+                            ),
+                          )
+                        ],
+                      ),
 
-class MySearchDelegate extends SearchDelegate {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          if (query.isEmpty) {
-            close(context, null);
-          } else {
-            query = '';
-          }
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return Container();
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Container();
+                      DocumentsList(
+                        oggetto: ogg!,
+                        document: List.generate(
+                          SearchView.docList.length,
+                          (i) => Document(
+                            'Documento ' + SearchView.docList[i],
+                            SearchView.urlList[i],
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ],
+        ));
   }
 }
